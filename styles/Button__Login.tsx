b9d3a58282ef94b21beef17ce5b65e2fd05bace6
nicolas bertinat <nbertinatwork@gmail.com>
2024-06-24 12:39:12 -0300

    Responsive header ready

--- styles/Button__Login.tsx
+++ styles/Button__Login.tsx
@@ -23,5 +23,23 @@ filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
     &:hover {
         background-color: var(--primary-color, #5D5FEF);
     }
+
+
+      @media (max-width: 1440px) {
+    font-size: 30px;
+    width: 100px;
+    height: 40px;
+    font-size: 20px;
+    padding
+  }
+
+    @media (max-width: 1000px) {
+    font-size: 30px;
+    width: 80px;
+    height: 30px;
+    font-size: 15px;
+    padding
+  }
+
 `;
 
