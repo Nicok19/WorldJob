8724b7ed89ef96b79af3b4892d6f8c83c950b30f
nicolas bertinat <nbertinatwork@gmail.com>
2024-06-26 16:19:36 -0300

    Change bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@
 import styles from "./index.module.css";
 import '../styles/globals.css';
 import Header from "@/app/components/shared/header/header";
-import BackgroundGradient from "@/app/components/home/section__backgroundGradient/backgroundGradient";
-import PresentationProduct from "@/app/components/home/section__PresentationProduct/presentationProduct";
-import Solutions from "@/app/components/home/article__Solutions/solutions";
-import ShowData from "@/app/components/home/section__ShowData/showData";
-import Blog from "@/app/components/home/section__Blog/blog";
+import BackgroundGradient from "@/app/components/home/backgroundGradient/backgroundGradient";
+import PresentationProduct from "@/app/components/home/PresentationProduct/presentationProduct";
+import Solutions from "@/app/components/home/Solutions/solutions";
+import ShowData from "@/app/components/home/ShowData/showData";
+import Blog from "@/app/components/home/Blog/blog";
 import Footer from "@/app/components/shared/footer/footer";
 
 
