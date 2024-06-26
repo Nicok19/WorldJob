1032510ba94162c85e12b4affdb40860abc2921e
nicolas bertinat <nbertinatwork@gmail.com>
2024-06-26 20:15:43 -0300

    Arrange Styled Components

--- src/app/components/home/Solutions/solutions.tsx
+++ src/app/components/home/Solutions/solutions.tsx
@@ -1,13 +1,11 @@
 import style from './solutions.module.css';
 import Image from 'next/image';
-import { SectionTitle } from '../../../../../styles/H1__SectionTitle';
-import { WatchMore } from '../../../../../styles/Button__WatchMore';
 
  export default function Solutions() {
 
 return(
 <article className={style.generalContainer}>
-<SectionTitle className={style.generalContainer__title}>Solutions</SectionTitle>
+<div className={style.generalContainer__title}>Solutions</div>
 
 
 {/* 1st section */}
@@ -22,7 +20,7 @@ return(
 
 <h2>Chat with compatriots who live where you want to live</h2>
 <p>Chat with fellow countrymen who live in the city you want to go in a personalized way and discover how to plan your country change in the best possible way.</p>
-<WatchMore>Watch more</WatchMore>
+<div className={style.watchMore}>Watch more</div>
 </div>
 
 <video className={style.videoSolutions} width="400" height="431" autoPlay muted playsInline>
@@ -43,7 +41,7 @@ return(
 
 <h2>Find compatriots communities abroad</h2>
 <p>find communities of compatriots in the new country you want to go to live.</p>
-<WatchMore>Watch more</WatchMore>
+<div className={style.watchMore}>Watch more</div>
 </div>
 
 <video className={style.videoSolutions} width="400" height="431" autoPlay muted playsInline>
@@ -64,7 +62,7 @@ return(
 
 <h2>Apply for jobs and scholarships in your new country</h2>
 <p>Apply to job offers and study scholarships in the country where you want to live from the comfort of your home and with monitoring of each application in real time.</p>
-<WatchMore>Watch more</WatchMore>
+<div className={style.watchMore}>Watch more</div>
 </div>
 
 <video className={style.videoSolutions} width="400" height="431" autoPlay muted playsInline>
@@ -86,7 +84,7 @@ return(
 
 <h2>Get help with immigration procedures</h2>
 <p>Our team of professionals will advise you and take care of all the necessary immigration procedures to enter your new country legally and safely.</p>
-<WatchMore>Watch more</WatchMore>
+<div className={style.watchMore}>Watch more</div>
 </div>
 
 <video className={style.videoSolutions} width="400" height="431" autoPlay muted playsInline>
